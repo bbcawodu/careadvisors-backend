235bb6b6d2293572d7e980b6096c7784e85e9cd8
Bradley Awodu <Kirabee@pichealthserver.patientinnovationcenter.org>
2016-12-01 23:03:20 -0600

    -Updated settings.base

--- picbackend/settings/base.py
+++ picbackend/settings/base.py
@@ -65,7 +65,8 @@ TEMPLATE_DIRS = (
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
+        'DIRS': [os.path.join(PROJECT_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
