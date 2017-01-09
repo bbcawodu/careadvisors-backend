091b285b629629f234ebdb92ef7d62bc6b4e4c8b
Bradley Awodu <Kirabee@pichealthserver.patientinnovationcenter.org>
2017-01-09 11:32:36 -0600

    -Added picbackend/wsgi.py to .gitignore

--- picbackend/wsgi.py
+++ picbackend/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "picbackend.settings.development")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "picbackend.settings.local")
 
 application = get_wsgi_application()
 
