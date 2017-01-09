dcb5e09d4551216dc92171ed8bae5fec9cce032b
Bradley Awodu <Kirabee@pichealthserver.patientinnovationcenter.org>
2017-01-09 11:34:24 -0600

    -changed wsgi.py

--- picbackend/wsgi.py
+++ picbackend/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "picbackend.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "picbackend.settings.development")
 
 application = get_wsgi_application()
 
