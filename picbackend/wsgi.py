2ac3eca661b48e7c5a0b5cf312bc5598ed437145
Bradley Awodu <awodubradley@gmail.com>
2017-02-23 08:16:17 -0600

    - Changed picbackend.wsgi to point to development settings

--- picbackend/wsgi.py
+++ picbackend/wsgi.py
@@ -11,8 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "picbackend.settings.demo")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "picbackend.settings.development")
 
 application = get_wsgi_application()
 
