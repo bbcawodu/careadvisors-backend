e33249cf817ef14412fc62f577c2f0c6b924b4f2
Bradley Awodu <awodubradley@gmail.com>
2017-06-02 12:23:52 -0500

    - Edit to v2/plans to check performance of writing JSON to http response

--- picbackend/views/v2/utils/providers_plans_network_utils/plans_utils.py
+++ picbackend/views/v2/utils/providers_plans_network_utils/plans_utils.py
@@ -343,6 +343,7 @@ def delete_plan(response_raw_data, rqst_carrier_info, post_errors):
 
 def retrieve_id_plans(response_raw_data, rqst_errors, plans, rqst_plan_id, list_of_ids, include_summary_report=False, include_detailed_report=False):
     if rqst_plan_id == "all":
+        pass
         # all_plans = plans
         # plan_dict = {}
         # for plan in all_plans:
