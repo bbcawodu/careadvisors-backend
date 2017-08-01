be782cbbd39d2595c86f8da58375bbaa1a66607c
Bradley Awodu <awodubradley@gmail.com>
2017-08-01 05:30:59 -0500

    - Removed code that adds primary and secondary guardian info to consumers

--- picmodels/services/base.py
+++ picmodels/services/base.py
@@ -1,6 +1,6 @@
 def filter_db_queryset_by_id(db_queryset, rqst_id, list_of_ids):
     if isinstance(rqst_id, str) and rqst_id.lower() == "all":
-        db_queryset = db_queryset.order_by("id")
+        db_queryset = db_queryset.all().order_by("id")
     else:
         db_queryset = db_queryset.filter(id__in=list_of_ids).order_by("id")
 
