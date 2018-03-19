84029ea57f9addb18e38f7fc9cd54f811f0676db
Bradley Awodu <awodubradley@gmail.com>
2018-03-19 11:04:05 -0500

    - fixed bugs in v2/provider_locations - Updated tests

--- picbackend/views/utils/base.py
+++ picbackend/views/utils/base.py
@@ -207,7 +207,7 @@ def clean_string_value_from_dict_object(dict_object, dict_name, dict_key, post_e
         post_errors.append("Value for {!r} in {!r} object is an empty string".format(dict_key, dict_name))
     elif dict_object[dict_key] is None and none_allowed is False:
         post_errors.append("Value for {!r} in {!r} object is Null".format(dict_key, dict_name))
-    elif not none_allowed and not isinstance(dict_object[dict_key], str):
+    elif (dict_object[dict_key] is None and none_allowed is False) and not isinstance(dict_object[dict_key], str):
         post_errors.append("Value for {!r} in {!r} object is not a string".format(dict_key, dict_name))
     else:
         return dict_object[dict_key]
@@ -234,7 +234,7 @@ def clean_int_value_from_dict_object(dict_object, dict_name, dict_key, post_erro
             post_errors.append("{!r} key not found in {!r} object".format(dict_key, dict_name))
     elif dict_object[dict_key] is None and not none_allowed:
         post_errors.append("Value for {!r} in {!r} object is Null".format(dict_key, dict_name))
-    elif not isinstance(dict_object[dict_key], int):
+    elif (dict_object[dict_key] is None and none_allowed is False) and not isinstance(dict_object[dict_key], int):
         post_errors.append("Value for {!r} in {!r} object is not an integer".format(dict_key, dict_name))
     else:
         return dict_object[dict_key]
@@ -261,7 +261,7 @@ def clean_float_value_from_dict_object(dict_object, dict_name, dict_key, post_er
             post_errors.append("{!r} key not found in {!r} object".format(dict_key, dict_name))
     elif dict_object[dict_key] is None and not none_allowed:
         post_errors.append("Value for {!r} in {!r} object is Null".format(dict_key, dict_name))
-    elif not isinstance(dict_object[dict_key], float) and not isinstance(dict_object[dict_key], int):
+    elif (dict_object[dict_key] is None and none_allowed is False) and not isinstance(dict_object[dict_key], float) and not isinstance(dict_object[dict_key], int):
         post_errors.append("Value for {!r} in {!r} object is not an float".format(dict_key, dict_name))
     else:
         return float(dict_object[dict_key])
@@ -288,7 +288,7 @@ def clean_dict_value_from_dict_object(dict_object, dict_name, dict_key, post_err
             post_errors.append("{!r} key not found in {!r} object".format(dict_key, dict_name))
     elif dict_object[dict_key] is None and none_allowed == False:
         post_errors.append("Value for {!r} in {!r} object is Null".format(dict_key, dict_name))
-    elif not none_allowed and not isinstance(dict_object[dict_key], dict):
+    elif (dict_object[dict_key] is None and none_allowed is False) and not isinstance(dict_object[dict_key], dict):
         post_errors.append("Value for {!r} in {!r} object is not a dictionary".format(dict_key, dict_name))
     elif dict_object[dict_key] == {}:
         post_errors.append("Value for {!r} in {!r} object is an empty dictionary".format(dict_key, dict_name))
