2a0111baba100a7269f564414fea24e4867adabb
Bradley Awodu <awodubradley@gmail.com>
2018-03-27 08:39:51 -0500

    - Since the HTTP protocol Same Origin Policy blocks access of cross domain cookies even when CORS is enabled, the frontend/client servers cannot get the csrftoken from the cookies of a backend server response. As an alternative, middleware is added to add a custom X-CSRFTOKEN header to all responses that is only viewable by CORS trusted domains

--- picbackend/settings/demo.py
+++ picbackend/settings/demo.py
@@ -8,3 +8,5 @@ HOSTURL = "https://metricsbackend-demo.herokuapp.com"
 if DEBUG:
     CORS_ORIGIN_WHITELIST += ('localhost:8080', 'localhost:5000', 'localhost:3000')
     CSRF_TRUSTED_ORIGINS += ('localhost:8080', 'localhost:5000', 'localhost:3000')
+
+CORS_ORIGIN_ALLOW_ALL = True
