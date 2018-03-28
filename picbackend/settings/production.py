30ca137c399dbd1fb99e52982d4e8bb750ee9f74
Bradley Awodu <awodubradley@gmail.com>
2018-03-28 09:58:54 -0500

    - Updated production settings

--- picbackend/settings/production.py
+++ picbackend/settings/production.py
@@ -6,15 +6,7 @@ DEBUG = False
 HOSTURL = "https://picbackend.herokuapp.com"
 
 if DEBUG:
-    CORS_ORIGIN_WHITELIST += ('localhost:8080', 'localhost:5000', 'localhost:3000',
-                              'localhost:8080/', 'localhost:5000/', 'localhost:3000/')
-    CSRF_TRUSTED_ORIGINS += ('localhost:8080', 'localhost:5000', 'localhost:3000',
-                             'localhost:8080/', 'localhost:5000/', 'localhost:3000/')
+    CORS_ORIGIN_WHITELIST += ('localhost:8080', 'localhost:5000', 'localhost:3000')
+    CSRF_TRUSTED_ORIGINS += ('localhost:8080', 'localhost:5000', 'localhost:3000')
     CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_WHITELIST += ('localhost:8080', 'localhost:5000', 'localhost:3000',
-                              # 'localhost:8080/', 'localhost:5000/', 'localhost:3000/'
-                          )
-CSRF_TRUSTED_ORIGINS += ('localhost:8080', 'localhost:5000', 'localhost:3000',
-                         # 'localhost:8080/', 'localhost:5000/', 'localhost:3000/'
-                         )
